3589cd65a31c042733fc7a8dcc3c54402394637c
Balasubramanian T K <47392334+btkcodedev@users.noreply.github.com>
2020-09-16 18:35:30 +0530

    Fixed the ge

--- gecwebscrap.py
+++ gecwebscrap.py
@@ -1,4 +1,5 @@
-#code not complete, error while parsing the html
+from lxml import html
+import requests
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
 
@@ -14,15 +15,22 @@ def render_page(url):
 
     r = render_page(myurl)
 """
+
 myurl= 'http://gecskp.ac.in'
 ucli= ureq(myurl)
 page_html = ucli.read()
 ucli.close()
 
 page_soup = soup(page_html,"html.parser")
-containers = page_soup.findAll("marquee")
-print(containers)
+containers = page_soup.findAll("a",{"style":"font-weight: 600; font-style: normal;"},{"href"})
 
+q=0
+for container in containers:
+    link = containers[q].get('href')
+    news = containers[q].text
+    orglink = ("http://gecskp.ac.in/"+link)
+    print("News: " + news+ " || Link: " + orglink + "\n")
+    q=q+1
 """
 filname='clgscrap.csv'
 f= open(filename,"w+")
@@ -32,3 +40,9 @@ f.write(headers)
 f.close()
 """
 
+"""
+page=requests.get("http://gecskp.ac.in")
+page_content=html.fromstring(page.content)
+items=page_content.xpath('//li/text()')
+print(items)
+"""
