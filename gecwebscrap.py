da6757726cd3bfa63163d7ea8c5ad4c98cf1be2a
Balasubramanian T K <tk_balu2000@outlook.com>
2021-02-04 13:21:41 +0530

    updated mail

--- gecwebscrap.py
+++ gecwebscrap.py
@@ -1,18 +1,18 @@
-import requests     #Helps to send HTTP request(connections)
-from urllib.request import urlopen as ureq      #Helps to get connected with Specified URL's
-from bs4 import BeautifulSoup as soup     #Helps to extract webpages using HTML Parser function
-import smtplib     #Helps to send out mails using SMTP
-import io      #Helps tp encode the characters into specified format
-import csv      #Helps to read csv file
-#opens up the web connection with specified URL
+import requests                               #Helps to send HTTP request(connections)
+from urllib.request import urlopen as ureq    #Helps to get connected with Specified URL's
+from bs4 import BeautifulSoup as soup         #Helps to extract webpages using HTML Parser function
+import smtplib,ssl                            #Helps to send out mails using SMTP
+import io                                     #Helps tp encode the characters into specified format
+import csv                                    #Helps to read csv file
+from email.mime.text import MIMEText          #Email Decoration
+from email.mime.multipart import MIMEMultipart
 
+#username : testscrapmailer@gmail.com pass: testscraper
 
-
-myurl= 'http://gecskp.ac.in'   #provide the required URL
-ucli= ureq(myurl)
+myurl= 'http://gecskp.ac.in' #provide the required URL
+ucli= ureq(myurl)            #Connect to webpage using URLLIB
 page_html = ucli.read()      #reads the webpage and transfers to a variable
-ucli.close()        #We should close the connection as the webpage can contain timeout
-
+ucli.close()                 #We should close the connection as the webpage can contain timeout
 
 
 #Get the html part using the html.parser function  of beautifulsoup library
@@ -23,58 +23,78 @@ containers = page_soup.findAll("a",{"style":"font-weight: 600; font-style: norma
 
 #opens a file to save the results
 filename='clgscrap.csv'
-with open(filename,"w+",encoding="utf-16") as f:     # as the file contains malayalam, specifying an external encoding is needed
+with open(filename,"w+",encoding="utf-16") as f:  # as the file contains malayalam, specifying an external encoding is needed
     headers= "NEWS,LINK\n"
-    f.write(headers)      #specifying headers
-
-    q=0
+    f.write(headers)                              #specifying headers
+    lis=[] 
+    q=0 
+    i=0
     for container in containers:
-        link = containers[q].get('href')      #Get the exact location inside the brief location
-        news = containers[q].text         #Get the text inside the tags using .text function
+        link = containers[q].get('href')         #Get the exact location inside the brief location
+        news = containers[q].text                #Get the text inside the tags using .text function
         orglink = ("http://gecskp.ac.in/"+link)  #Get the link
         if(q==1):
             storage=(news.replace(",","|")+" "+orglink.replace(",","|") + "\n")
-        print("News: " + news+ " || Link: " + orglink + "\n") 
-        f.write(news.replace(",","|")+" "+orglink.replace(",","|") + "\n")  #Writing in the file       #while Writing,the comma represents the next column
+        final="News: " + news+ "\n"
+        link="Link: " + orglink + "\n"
+        lis=lis+[final+link] 
+        f.write(news.replace(",","|")+" "+orglink.replace(",","|") + "\n")  #Writing in the file.while Writing,the comma represents the next column
         q=q+1
+        i=i+1
+f.close()                                          #finally close the file to open it with interface
+print("Latest\n",lis[1])
+print("Previous\n",lis[2])
+print("All\n",' '.join(lis))
 
-f.close()  #finally close the file to open it with interface
+smtp_server = "smtp.gmail.com" 
+port = 587                              # For starttls
+sender="testscrapmailer@gmail.com"
+appkey=input("Enter Password:")
+receiver="farij24645@botfed.com"
+context = ssl.create_default_context()
 
+message = MIMEMultipart("alternative")
+message["Subject"] = "GEC SCRAPER ALERT"
+message["From"] = sender
+message["To"] = receiver
 
+# Create the plain-text and HTML version of message
+text = """\
+GEC SCRAPER: 
+Dear Student,
+The website has been updated by a new notification as follows
+""" +str(lis[1])+ """ <br> Previous Mail: """ +str(lis[2])+ """
 """
-Uses the encoding as utf-16 to get malayalam and english 
-with open(filename,"r+",encoding="utf-16") as f:
-    reader = csv.reader(f)
-    row1=next(reader)
-    row2=next(reader)
-    row3=next(reader)
-f.close()
 
-print(row3)
-print(storage)
-if(row3==storage):
-    print("yes")
-else:
-    print("No")
-"""    
-    
-    
-   
+html = """\
+<html>
+  <body>
+    <h4 style="text-align:center;">GEC SCRAPER</h4>
+    <h5><p><li>Dear Student,</li></h5>
+    <br>The website has been updated by a new notification as follows,<br>
+    """ +str(lis[1])+ """ <br> Previous Mail: """ +str(lis[2])+ """
+    </p>
+    <h4>Thanks</h4>
+    <h6>For Unsubscribing, Click <a href="#">here</a></h6>
+  </body>
+</html>
 """
-This uses selenium
-page=requests.get("http://gecskp.ac.in")
-page_content=html.fromstring(page.content)
-items=page_content.xpath('//li/text()')
-print(items)
 
-import csv
-from lxml import html
-from selenium import webdriver
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
 
-def render_page(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    r = driver.page_source
-    return r
-    r = render_page(myurl)
-"""
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
+
+try:
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as conn:
+      conn.login(sender, appkey)
+      conn.sendmail(
+          sender, receiver, message.as_string()
+      )
+except Exception as e:
+  print(e)
+  conn.quit()
